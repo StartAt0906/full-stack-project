602e9fd2571b64175666bdd7237a08d05bb70931
Jane Lee <19537504991@163.com>
2026-09-06 18:35:42 +0800

    “test: 首次在IDEA里提交”

--- sb-ecom/src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
@@ -147,40 +147,7 @@ public class OrderServiceImpl implements OrderService {
         return modelMapper.map(order, OrderDTO.class);
     }
 
-//    @Override
-//    public OrderResponse getAllSellerOrders(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
-//        Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
-//                ? Sort.by(sortBy).ascending()
-//                : Sort.by(sortBy).descending();
-//        Pageable pageDetails = PageRequest.of(pageNumber, pageSize, sortByAndOrder);
-//
-//        User seller = authUtil.loggedInUser();
-//
-//        Page<Order> pageOrders = orderRepository.findAll(pageDetails);
-//
-//        List<Order> sellerOrders = pageOrders.getContent().stream()
-//                .filter(order -> order.getOrderItems().stream()
-//                        .anyMatch(orderItem-> {
-//                            var product = orderItem.getProduct();
-//                            if (product == null || product.getUser() == null) {
-//                                return false;
-//                            }
-//                            return product.getUser().getUserId().equals(seller.getUserId());
-//                        }))
-//                .toList();
-//        List<OrderDTO> orderDTOS = sellerOrders.stream()
-//                .map(order -> modelMapper.map(order, OrderDTO.class))
-//                .toList();
-//        OrderResponse orderResponse = new OrderResponse();
-//        orderResponse.setContent(orderDTOS);
-//        orderResponse.setPageNumber(pageOrders.getNumber());
-//        orderResponse.setPageSize(pageOrders.getSize());
-//        orderResponse.setTotalElements(pageOrders.getTotalElements());
-//        orderResponse.setTotalPages(pageOrders.getTotalPages());
-//        orderResponse.setLastPage(pageOrders.isLast());
-//        return orderResponse;
-//    }
-//
+
 
     public OrderResponse getAllSellerOrders(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
         Sort sortByAndOrder = sortOrder.equalsIgnoreCase("asc")
